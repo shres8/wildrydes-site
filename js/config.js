e3064b3a23f13cd474bdf57567347f1b5350fc3c
shres8 <93535344+shres8@users.noreply.github.com>
2024-12-18 12:28:29 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_xwJA5d4co', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '41euc84ur9du61mksd08gglvrd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_WZokJhs8n', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '491i9fe4q3q2evs0mi6cq8f26k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
